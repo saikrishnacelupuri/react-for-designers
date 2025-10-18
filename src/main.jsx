d1b44f8b0b7dca426e89ab2842efcdff090c6c3e
saikrishnacelupuri <krishnacelupuri@gmail.com>
2025-10-18 13:13:16 +0100

    Initital React setup with vite and pure javascript template

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// This is the entry point of the react application
+
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
